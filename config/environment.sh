146f3cc60293a2ff60679fbff58671c235dfe497
James R. Heselden <34611419+Iranaphor@users.noreply.github.com>
2025-10-15 17:06:39 +0100

    Initialize riseholme_park_farm_field_5

---
+++ config/environment.sh
@@ -0,0 +1,16 @@
+# Farm Details
+export FARM_NAME='riseholme_park_fields'
+
+# Field Details
+export FIELD_NAME='field_7'
+export FIELD_ID='uuid'
+export FIELD_TYPE='pasture'
+
+# Config Quick-References
+#CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+
+#export DATUM_FILE="$CONF/location/datum.yaml"
+#if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
+
+#export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
+#if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
