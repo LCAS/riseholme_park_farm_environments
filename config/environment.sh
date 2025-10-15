520717302732fb1cc313cbdc7a880018cfb34674
James R. Heselden <34611419+Iranaphor@users.noreply.github.com>
2025-10-15 17:06:35 +0100

    Initialize riseholme_park_farm_walled_garden

---
+++ config/environment.sh
@@ -0,0 +1,16 @@
+# Farm Details
+export FARM_NAME='riseholme_park'
+
+# Field Details
+export FIELD_NAME='walled_garden'
+export FIELD_ID='g1'
+export FIELD_TYPE='garden'
+
+# Config Quick-References
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config
+
+export DATUM_FILE="$CONF/location/datum.yaml"
+if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
+
+export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
+if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
