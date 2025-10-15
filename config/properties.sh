333bace438b1a965a865fd2ae9b3d1e37cbc8a0e
James R. Heselden <34611419+Iranaphor@users.noreply.github.com>
2025-10-15 17:07:37 +0100

    Initialize riseholme_park_farm_vineyard

---
+++ config/properties.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#================================================================
+# HEADER
+#================================================================
+#% DESCRIPTION
+#%    This is a source file to enable quick and simple access to
+#%    basic properties about the environment.
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         environment_template | properties.sh (1.0)
+#-    author          James R. Heselden
+#-    license         Apache License Version 2.0
+#-
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+
+#================================================================
+# Origin Details
+#================================================================
+#% DESCRIPTION
+#%    Section to define the origin of the maps if one exists
+#================================================================
+export ENVIRONMENT_ORIGINS=''
+
+
+#================================================================
+# Farm Details
+#================================================================
+#% DESCRIPTION
+#%    Used to identify the associated farm.
+#================================================================
+export FARM_NAME='riseholme_park_farm'
+
+
+
+#================================================================
+# Field Details
+#================================================================
+#% DESCRIPTION
+#%    Used to identify the associated field.
+#================================================================
+export FIELD_NAME='vineyard'
+export FIELD_ID='v1'
+export FIELD_TYPE='vineyard'
