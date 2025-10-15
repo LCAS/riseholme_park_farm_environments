ced4715a75eebc4d32efa7a6fe84bb7d11689b04
James R. Heselden <34611419+Iranaphor@users.noreply.github.com>
2025-10-15 17:07:40 +0100

    Initialize riseholme_park_farm_briary_hill

---
+++ config/properties.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#================================================================
+# HEADER
+#================================================================
+#% DESCRIPTION
+#%    This is a source file to enable quick and simple access to
+#%    basic properties about the environment.
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         environment_template | properties.sh (1.0)
+#-    author          James R. Heselden
+#-    license         Apache License Version 2.0
+#-
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+
+#================================================================
+# Origin Details
+#================================================================
+#% DESCRIPTION
+#%    Section to define the origin of the maps if one exists
+#================================================================
+export ENVIRONMENT_ORIGINS='google_earth'
+
+
+#================================================================
+# Farm Details
+#================================================================
+#% DESCRIPTION
+#%    Used to identify the associated farm.
+#================================================================
+export FARM_NAME='riseholme_pack_farm'
+
+
+
+#================================================================
+# Field Details
+#================================================================
+#% DESCRIPTION
+#%    Used to identify the associated field.
+#================================================================
+export FIELD_NAME='briary_hill'
+export FIELD_ID='bh1'
+export FIELD_TYPE='woodland'
